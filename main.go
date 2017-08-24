5ae16429364440480ea256652cb17a87af90078c
Dzyanis Kuzmenka <dzyanis.kuzmenka@kufar.by>
2017-08-25 00:44:08 +0300

    update redis host

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	client := redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
+		Addr:     "redis:6379",
 		Password: "", // no password set
 		DB:       0,  // use default DB
 	})
